ec1eec2c38ccdf6dc288449699d99ff6ae9a178e
tharunoptimus <tharunoptimus@outlook.com>
2022-01-06 16:17:37 +0530

    Changed homepage

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -1,70 +1,74 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react"
+import clsx from "clsx"
+import styles from "./HomepageFeatures.module.css"
 
 type FeatureItem = {
-  title: string;
-  image: string;
-  description: JSX.Element;
-};
+	title: string
+	image: string
+	description: JSX.Element
+}
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    image: '/img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    image: '/img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    image: '/img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
+	{
+		title: "Easy to Use",
+		image: "/img/relieved-face.svg",
+		description: (
+			<>
+				Infant 👶 Lang is a programming language that is designed to be
+				easy to learn and easy to use. Inspired by Brain Fuck 🧠, it is
+				less esoteric and more fun to learn.
+			</>
+		),
+	},
+	{
+		title: "Lightning Fast Interpreter",
+		image: "/img/lightning.svg",
+		description: (
+			<>
+				The Interpreter for Infant Lang is written in Go, the C for the
+				21st century 🥇. The Interpreter is super fast ⚡ and with the
+				power of VSCode extensions, it is extremely 🦸‍♀️ easy to get
+				started with.
+			</>
+		),
+	},
+	{
+		title: "Aimed at Infants",
+		image: "/img/baby.svg",
+		description: (
+			<>
+				Not a joke, the syntax is super easy that a 3 year old 👶 can
+				easily learn 🏫. It has basic things such as a moveable pointer 👆, a
+				memory box 📦 and some print statements 🖨
+			</>
+		),
+	},
+]
 
-function Feature({title, image, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img className={styles.featureSvg} alt={title} src={image} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, image, description }: FeatureItem) {
+	return (
+		<div className={clsx("col col--4")}>
+			<div className="text--center">
+				<img className={styles.featureSvg} alt={title} src={image} />
+			</div>
+			<div className="text--center padding-horiz--md">
+				<h3>{title}</h3>
+				<p>{description}</p>
+			</div>
+		</div>
+	)
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	)
 }
